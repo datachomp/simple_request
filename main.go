755c3ce230da1a824b5b41f9c8dec5aff49e00cf
rob sullivan <rob@datachomp.com>
2015-09-23 12:12:19 -0500

    basic listener

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+  log.Println("hit on %s",  r.URL.Path[1:])
+    fmt.Fprintf(w, "Welcome to: %s!", r.URL.Path[1:])
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    log.Println("Listening on localhost:8282")
+    http.ListenAndServe(":8282", nil)
+}
